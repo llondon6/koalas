d70985eac5063c641ff77b1d7545d16e83930682
Edward Fauchon-Jones <edward.fauchon.jones@gmail.com>
2019-04-04 17:35:30 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as f:
 #
 setup(
     name='positive',
-    version='0.1.0',
+    version='0.2.0',
     description='Low level python API for NR+LIGO packages.',
     long_description=readme,
     long_description_content_type='text/markdown',
