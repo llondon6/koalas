59773adcc28953919d60e330182d06cb12d4ee61
Lionel London <lionel.london@ligo.org>
2019-11-26 14:55:32 +0000

    updating setup file for current release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r') as f:
 #
 setup(
     name='positive',
-    version='0.2.0',
+    version='0.2.2',
     description='Low level python API for NR+LIGO packages.',
     long_description=readme,
     long_description_content_type='text/markdown',
